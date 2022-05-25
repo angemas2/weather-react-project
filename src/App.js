bb397bc637079362d7bacdbd6f64a79d520405a8
angemas <angelique.massolo@outlook.com>
2022-05-25 21:07:15 +0200

    set up projet and bootstrap

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h1>Weather app</h1>
+        <button className="btn btn-primary">Hello</button>
+      </div>
     </div>
   );
 }
-
-export default App;
