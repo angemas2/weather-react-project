381c6582332c18de283a09ad9eec33f37c7953f3
angemas <angelique.massolo@outlook.com>
2022-05-31 19:09:14 +0200

    icon animated

--- src/Weatherdata.js
+++ src/Weatherdata.js
@@ -1,26 +1,21 @@
 import React from "react";
+import Weathericon from "./Weathericon";
 
 
 
 export default function Weatherdata(props){
     return (
       <div className="currentWeather mt-5">
-        <h1>
-          {props.data.city}
-        </h1>
         <div class="row">
           <div class="col tempdesc">
+            <h4>{props.data.city}</h4>
             <div className="currentTemp">{Math.round(props.data.temp)}°C</div>
             <div className="currentDesc text-capitalize">
               {props.data.description}
             </div>
           </div>
-          <div class="col">
-            <img
-              src={props.data.icon}
-              alt={props.data.description}
-              width="100px"
-            />
+          <div class="col canvas">
+            <Weathericon code={props.data.icon} />
           </div>
         </div>
         <ul className="currentDetails mt-4">
